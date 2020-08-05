e3e465999ad7f05b69661c7f87ce9e7eb8e1ec5f
Mubashir-M <mubashir.mohamud@gmail.com>
2020-08-05 21:21:54 +0300

    Done up to 3.4

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const { response } = require('express')
+const app = express()
+
+  app.use(express.json()) 
+
+  
+
+let contacts = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id:1
+
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id:2
+
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id:3
+
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id:4
+
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(contacts)
+  })
+
+app.get('/info', (req,res) => {
+    const count = contacts.length
+    const date = new Date()
+    console.log('count and date',count,date)
+    res.send(`<p>Phonebook has info for ${count} people<br>${date}}</p>`)
+})
+
+app.get('/api/persons/:id',(req,res) => {
+    const id = Number (req.params.id)
+    const contact = contacts.find(contact => contact.id === id)
+
+    if (contact) {
+        res.json(contact)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id',(req,res) => {
+    const id = Number(req.params.id)
+    contacts = contacts.filter(contact => contact.id !== id)
+    res.status(404).end()
+})
+
+
+
+
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
