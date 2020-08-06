ce3ad30636363ff1f1015ef602f26197074dd589
Mubashir-M <mubashir.mohamud@gmail.com>
2020-08-06 20:51:05 +0300

    Completed 3.8

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
-const { response } = require('express')
+const { response, json } = require('express')
 const app = express()
+const morgan = require('morgan')
 
-  app.use(express.json()) 
+    app.use(express.json())
 
+    morgan.token("data",(req,res) =>{
+        return JSON.stringify({
+            name:req.body.name,
+            number: req.body.number
+        })
+    })
+    
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+    
+    
   
 
 let contacts = [
@@ -55,11 +66,45 @@ app.get('/api/persons/:id',(req,res) => {
     }
 })
 
+
+app.post('/api/persons', (req,res) => {
+    const GeneratedId = Math.floor(Math.random() * 100000);
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }
+    else if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }
+    else if (contacts.filter(contact => contact.name.toLowerCase() === body.name.toLowerCase()).length>0){
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const contact = {
+        name:body.name,
+        number: body.number,
+        id : GeneratedId
+    }
+
+    contacts = contacts.concat(contact)
+    res.json(contact)
+
+})
+
 app.delete('/api/persons/:id',(req,res) => {
     const id = Number(req.params.id)
     contacts = contacts.filter(contact => contact.id !== id)
     res.status(404).end()
 })
+
+
 
 
 
