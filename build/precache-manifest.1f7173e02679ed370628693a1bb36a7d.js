019fec290f33c06d94a361c76af2307b9271376f
Mubashir-M <mubashir.mohamud@gmail.com>
2020-08-11 19:18:36 +0300

    uibuild

---
+++ build/precache-manifest.1f7173e02679ed370628693a1bb36a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81b9bbed4471eafcab16a33241440cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f4113c3124d5d308f2",
+    "url": "/static/css/main.af1f6cf8.chunk.css"
+  },
+  {
+    "revision": "7db88a272722b7822afa",
+    "url": "/static/js/2.592545cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.592545cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f4113c3124d5d308f2",
+    "url": "/static/js/main.8ad756d3.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
