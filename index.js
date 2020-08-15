acea0ba5bd37cc11e47ef0a1b6ebfe4fd0c46136
Mubashir-M <mubashir.mohamud@gmail.com>
2020-08-15 19:12:26 +0300

    uibuild

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require('cors')
 const Note = require('./models/note')
 
 
-    app.use(express.static('build'))
-    app.use(express.json())
-    app.use(cors())
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
     morgan.token("data",(req,res) =>{
         return JSON.stringify({
@@ -64,11 +64,16 @@ app.get('/info', (req,res) => {
     
 })
 
-app.get('/api/persons/:id',(req,res) => {
+app.get('/api/persons/:id',(req,res,next) => {
     
     Note.findById(req.params.id).then(note => {
-        res.json(note.toJSON())
-      })
+        if (note) {
+            res.json(note)
+          } else {
+            res.status(404).end()
+          }
+        })
+        .catch(error => next(error))
       
 })
 
@@ -107,11 +112,29 @@ app.post('/api/persons', (req,res) => {
 
 })
 
-app.delete('/api/persons/:id',(req,res) => {
-    const id = Number(req.params.id)
-    contacts = contacts.filter(contact => contact.id !== id)
-    res.status(404).end('Deleted conctact successfully')
+app.delete('/api/persons/:id',(req,res,next) => {
+    Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end('Deleted contact successfully')
+    })
+    .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    console.log(body)
+
+    const note = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Note.findByIdAndUpdate(req.params.id, note)
+      .then(updatedNote => {
+        res.json(updatedNote.toJSON())
+      })
+      .catch(error => next(error))
+  })
 
 
 
